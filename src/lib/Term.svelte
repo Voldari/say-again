de1b1a376c98dee05c03b3b274a2059cd6b88d25
Isaac Harper <89951169+Voldari@users.noreply.github.com>
2022-10-26 14:05:58 -0700

    term

--- src/lib/Term.svelte
+++ src/lib/Term.svelte
@@ -1,19 +1,21 @@
 <div class="border search">
-
+    search
 </div>
 
 <div class="border container">  
-
+    def
 </div>  
 
 <style> 
     
     .search {
         margin: .25rem .5rem;
+        grid-area: 5 / 1 / 6 / 3;
     }
     
     .container {
         margin: .25rem .5rem;
+        grid-area: 6 / 1 / 7 / 3;
     }  
     
  </style> 
