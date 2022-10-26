443c775cb4a8dd3ccca9b09b77a47258fdf64259
Isaac Harper <89951169+Voldari@users.noreply.github.com>
2022-10-26 13:35:43 -0700

    join

--- src/lib/JoinDictionary.svelte
+++ src/lib/JoinDictionary.svelte
@@ -1,11 +1,38 @@
-<div class="container">  
+<script lang="ts">
+    let selected = false
+    
+    function toggle() {
+        selected = !selected
+    }
+</script>
 
-</div>  
+<div class="toggle border" on:click={toggle}>
+    {#if selected}
+        Cancel
+    {:else}
+        Join
+    {/if}
+</div>
+
+{#if selected}
+    <div class="border container">  
+        howdy
+    </div>  
+{/if}
 
 <style> 
 
-    .container {
+    .toggle {
+        grid-area: 2 / 1 / 3 / 2 ;
+    }
     
+    .container {
+        grid-area: 3 / 1 / 4 / 3 ;
     }  
     
+    .selected {
+        background-color: var(--red);
+    }
+    
  </style> 
+
