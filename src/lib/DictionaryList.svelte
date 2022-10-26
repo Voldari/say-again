ab459b5784afdb5aa04e714920458ab3421b320e
Isaac Harper <89951169+Voldari@users.noreply.github.com>
2022-10-26 14:16:02 -0700

    list

--- src/lib/DictionaryList.svelte
+++ src/lib/DictionaryList.svelte
@@ -1,9 +1,31 @@
-<div class="border container">  
-    rofl
+<div class="border container horizontal_list">  
+    
+    <div class="border horizontal_list_item">Item</div>
+    <div class="border horizontal_list_item">Item</div>
+    <div class="border horizontal_list_item">Item</div>
+    <div class="border horizontal_list_item">Item</div>
+    <div class="border horizontal_list_item">Item</div>
 </div>  
 
 <style> 
     .container {
         grid-area: 4 / 1 / 5 / 3
     }  
+    
+    
+
+  .horizontal_list {
+      display: flex;
+      overflow-x: scroll;
+
+      grid-area: 2 / 1 / 3 / 4; 
+  }
+
+  .horizontal_list_item {
+      flex: 0 0 8rem;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      margin: 0 .25rem;
+  }
  </style> 
